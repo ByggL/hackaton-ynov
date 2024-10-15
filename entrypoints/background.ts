aab0bcda3fca8200935a73a6680cee25306fe22f
Samuel Léobon <76527824+ByggL@users.noreply.github.com>
2024-10-15 10:57:11 +0200

    background fetching and processing

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,42 @@
+import data from './testData.json';
+
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if(request.type === "fetch_api") {
+
+      browser.storage.sync.get("doneList").then((res) => {
+        let list = res.doneList;
+
+        const formattedData = data.map((item: any) => {
+          return list[item.title] ? {...item, is_done: list[item.title]} : {...item, is_done: false};
+        })
+
+        console.log(JSON.stringify(formattedData, null, 4))
+        browser.runtime.sendMessage({type: "fetch_response", data: formattedData})
+      }).catch(()=> {
+        const formattedData = data.map((item: any) => {
+          return {...item, is_done: false};
+        })
+
+        console.log(JSON.stringify(formattedData, null, 4))
+        browser.runtime.sendMessage({type: "fetch_response", data: formattedData})
+      })
+
+      return true;
+    }
+
+    if(request.type === "toggle_done") {
+      browser.storage.sync.get("doneList").then((res) => {
+
+        let list = res.doneList;
+        list[request.title] = true;
+
+        browser.storage.sync.set({doneList: list});
+      });
+
+      return true;
+    }
+
+  return true;
+  })
 });
--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,10 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import reactLogo from '@/assets/react.svg';
 import wxtLogo from '/wxt.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  /////////// STATE VARIABLES //////////
+  const [backendData, setBackendData] = useState();
+  //////////////////////////////////////
+
+  // TO BE USED FOR CALLING BACKEND
+  useEffect(() => {
+    browser.runtime.sendMessage({type: "fetch_api"});
+  }, []);
+
+  browser.runtime.onMessage.addListener(function(request,sender, sendResponse) {
+    if(request.type === "fetch_response") {
+      setBackendData(request.data);
+    }
+  })
+  
 
   return (
     <>
@@ -18,11 +32,8 @@ function App() {
       </div>
       <h1>WXT + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          {JSON.stringify(backendData)}
         </p>
       </div>
       <p className="read-the-docs">
